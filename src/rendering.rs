58d48152e903b6a43437f932acb24219deb62efe
gram <git@orsinium.dev>
2026-02-08 16:04:48 +0100

    refactor consts

--- src/rendering.rs
+++ src/rendering.rs
@@ -1,8 +1,14 @@
 use crate::*;
 use firefly_rust::*;
 
-const PAGE_MARGIN: i32 = 20;
-const CURSOR_MARGIN: i32 = 4;
+const BOX_ML: i32 = 20;
+const BOX_MR: i32 = BOX_ML;
+const BOX_MT: i32 = 20;
+const BOX_Y: i32 = BOX_MT;
+
+const CURSOR_ML: i32 = 4;
+const LINE_M: i32 = 4;
+const CURSOR_X: i32 = BOX_ML + CURSOR_ML;
 
 pub fn render_state(state: &State) {
     draw_bg(state);
@@ -18,7 +24,7 @@ fn draw_title(state: &State) {
     let font = state.font.as_font();
     let mut point = Point::new(
         (WIDTH - line_width(&font, title)) / 2,
-        PAGE_MARGIN + font.char_height() as i32,
+        BOX_Y + font.char_height() as i32,
     );
     if state.cursor == 0 && (state.btns.s || state.btns.e) {
         point.x += 1;
@@ -29,7 +35,7 @@ fn draw_title(state: &State) {
 
 fn draw_title_arrows(state: &State) {
     let style = Style::solid(state.theme.accent);
-    let mut p = Point::new(PAGE_MARGIN + CURSOR_MARGIN + 1, PAGE_MARGIN + 3);
+    let mut p = Point::new(CURSOR_X + 1, BOX_Y + 3);
     if state.cursor == 0 && (state.btns.s || state.btns.e) {
         p.x += 1;
         p.y += 1;
@@ -41,7 +47,7 @@ fn draw_title_arrows(state: &State) {
         style,
     );
 
-    p.x += WIDTH - 2 * (PAGE_MARGIN + CURSOR_MARGIN) - 3;
+    p.x += WIDTH - 2 * CURSOR_X - 3;
     draw_triangle(
         Point::new(p.x, p.y + 4),
         Point::new(p.x - 4, p.y),
@@ -52,12 +58,9 @@ fn draw_title_arrows(state: &State) {
 
 fn draw_lines(state: &State) {
     let font = state.font.as_font();
-    let line_h = font.char_height() as i32 + CURSOR_MARGIN;
+    let line_h = font.char_height() as i32 + LINE_M;
     for (line, i) in state.page.lines().iter().zip(2..) {
-        let mut point = Point::new(
-            PAGE_MARGIN + CURSOR_MARGIN,
-            PAGE_MARGIN + i * line_h - CURSOR_MARGIN,
-        );
+        let mut point = Point::new(CURSOR_X, BOX_Y + i * line_h - LINE_M);
         if i - 1 == state.cursor as i32 && (state.btns.s || state.btns.e) {
             point.x += 1;
             point.y += 1;
@@ -69,13 +72,10 @@ fn draw_lines(state: &State) {
 
 fn draw_cursor(state: &State) {
     let font = state.font.as_font();
-    let line_h = font.char_height() as i32 + CURSOR_MARGIN;
-    let y = PAGE_MARGIN + state.cursor as i32 * line_h + 1;
-    let mut point = Point::new(PAGE_MARGIN, y);
-    let bbox = Size::new(
-        WIDTH - PAGE_MARGIN * 2,
-        font.char_height() as i32 + CURSOR_MARGIN,
-    );
+    let line_h = font.char_height() as i32 + LINE_M;
+    let y = BOX_MT + state.cursor as i32 * line_h + 1;
+    let mut point = Point::new(BOX_ML, y);
+    let bbox = Size::new(WIDTH - BOX_ML - BOX_MR, font.char_height() as i32 + LINE_M);
     let corner = Size::new(4, 4);
 
     if state.btns.s || state.btns.e {
@@ -117,9 +117,9 @@ fn draw_lang_selection(state: &State) {
     };
 
     let font = state.font.as_font();
-    let x = WIDTH - PAGE_MARGIN - CURSOR_MARGIN - 7;
-    let line_h = font.char_height() as i32 + CURSOR_MARGIN;
-    let y = PAGE_MARGIN + CURSOR_MARGIN + idx * line_h;
+    let x = WIDTH - CURSOR_X - 7;
+    let line_h = font.char_height() as i32 + LINE_M;
+    let y = CURSOR_X + idx * line_h;
     let mut point = Point::new(x, y);
     if idx == state.cursor as i32 && (state.btns.s || state.btns.e) {
         point.x += 1;
